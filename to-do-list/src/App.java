27e09163e99c8935cac5b84c9d0feafd2bd2d7fb
O8S7K <aplotec.oj@gmail.com>
2025-06-28 19:45:07 +0200

    Add loop to add multiple todo's

--- to-do-list/src/App.java
+++ to-do-list/src/App.java
@@ -2,22 +2,33 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) throws Exception {
+    System.out.println("__ TODO LIST __");
+    Scanner sc = new Scanner(System.in);
 
-        Tarea tarea = new Tarea(1, "Tarea nueva", false);
-        
-        ArrayList<Tarea> tareas = new ArrayList<>();
+    String input;
+    int id = 1;
 
-        String in = sc.nextLine();
-        tarea.setContent(in);
+    ArrayList<Tarea> tareas = new ArrayList<>();
 
-        tareas.add(tarea);
+    System.out.println("\nAniade una tarea nueva");
+    System.out.println("Quieres guardar tus lista escribe 'save'");
 
-        for (Tarea t : tareas) {
-          System.out.println(t.getContent());
-          System.out.println(t.getId());
-        }
-        sc.close();
+    while (true) {
+      System.out.print("\nTarrea nueva: ");
+      input = sc.nextLine();
+      if (input.equals("save")) {
+        break;
+      }
+      Tarea nuevaTarea = new Tarea(id, input, false); // ✅ nueva instancia cada vez
+      tareas.add(nuevaTarea);
+      id++; // ✅ incrementar el ID después de añadir
     }
+
+    for (Tarea t : tareas) {
+      System.out.println("[" + t.getId() + "] " + t.getContent());
+    }
+
+    sc.close();
+  }
 }
