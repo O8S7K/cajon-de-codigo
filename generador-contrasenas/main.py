6bd6e7f19b6c50b64b9f3fc6e5aa47bc513e8715
O8S7K <aplotec.oj@gmail.com>
2025-06-30 19:24:54 +0200

    Basic strucutrue for generador-contrasenas

--- generador-contrasenas/main.py
+++ generador-contrasenas/main.py
@@ -1,1 +1,18 @@
+import random
+
 print("__Generador de contrasenas__")
+
+longitud = int(input("\nIntroduce una longitud de contrasena (Recomendacion minima 8 characteres)"))
+contrasenaNueva= ""
+characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+simbolos = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "?"]
+numeros = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+union = characters + simbolos + numeros
+random.shuffle(union)
+
+for i in range(0, longitud):
+    contrasenaNueva += union[i]
+
+print(contrasenaNueva)
+
