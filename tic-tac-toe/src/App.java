ee6a733364b341429494487db429f3bdfc02d66b
O8S7K <aplotec.oj@gmail.com>
2025-08-11 23:37:22 +0200

    new logic update array

--- tic-tac-toe/src/App.java
+++ tic-tac-toe/src/App.java
@@ -1,43 +1,53 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // Contruir un array bidimensional
- 
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Tic Tac Toe juego, cada casilla tiene dos numero corespodientes ");
-        System.out.println("[11][12][13]");
-        System.out.println("[21][22][23]");
-        System.out.println("[31][32][33]");
+        // Mostrar el tablero
 
+        var matrix = new String[3][3];
+        printMatrix(matrix);
 
-        // Permitir al usuario modificar matriz basado en su input
-        String[][] vacio = {{ "_", "_", "_" },
-                { "_", "_", "_" },
-                { "_", "_", "_" } };
+        int[] fields = userInput(sc);
 
-        for (int i = 0; i < vacio.length; i++) {
-            for (int j = 0; j < vacio[i].length; j++) {
-                System.out.print(vacio[i][j] + " ");
+        matrix[fields[0]][fields[1]] = "x" + " ";
+
+        updateMatrix(matrix);
+
+        // Entrada de usuario
+
+    }
+
+    public static void printMatrix(String[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] = "-" + " ");
             }
             System.out.println();
         }
+    }
 
-        Scanner sc = new Scanner(System.in);
+    public static void updateMatrix(String[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static int[] userInput(Scanner sc) {
         System.out.println("\nMarca el conjunto de numeros correpsodinete con el campo: ");
         int number = sc.nextInt();
 
-        double quotient = number/10.0;
+        double quotient = number / 10.0;
+        int firstNumber = (int) (Math.floor(quotient)) - 1;
+        int secondNumber = (int) Math.round((quotient - Math.floor(quotient)) * 10) - 1;
 
-        System.out.println(quotient+ " dsvsdfgbvgfs34");
-        int firstNumber = (int)(Math.floor(quotient));
-
-        System.out.println(firstNumber+ " hheh");
-
-        double secondNumber = Math.round((quotient - Math.floor(quotient))*10) ;
-
-        System.out.println(secondNumber+ " hheh");
-
-        sc.close();
+        int[] input = { firstNumber, secondNumber };
+        return input;
     }
 }
